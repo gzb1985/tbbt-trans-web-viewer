ccbc8aa5c216e8888f90294418c4db749632189e
Gong Zibin <gzb1985@gmail.com>
2013-03-23 23:08:18 +0800

    heroku relate files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn -w 4 -b 0.0.0.0:$PORT -k gevent app:app
+clock: python scripts/crawler_job.py
---
+++ app.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+import os
+from transviewer import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port,debug=True)
